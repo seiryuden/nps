52dfb4eb590985d2b78858141b66bd46968e6f9b
seiryuden <seiryuden@outlook.com>
2019-02-11 17:23:12 +0900

    v1

---
+++ index.js
@@ -0,0 +1,70 @@
+'use strict';
+
+// put your own value below!
+const apiKey = 'ALGP0raPtnXuYgjmOc7z1xtWBBnogfVmmnKdW9YU'; 
+const searchURL = 'https://api.nps.gov/api/v1/parks';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson) {
+  
+  console.log(responseJson);
+  $('#results-list').empty();
+  
+  for (let i = 0; i < responseJson.data.length; i++){
+  
+    $('#results-list').append(
+      `<li><h3>${responseJson.data[i].fullName}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <p>${responseJson.data[i].addresses[0].city}, ${responseJson.data[i].addresses[0].line1}, ${responseJson.data[i].addresses[0].line2}, ${responseJson.data[i].addresses[0].postalCode}, ${responseJson.data[i].addresses[0].stateCode} .</p>
+      <p><a href='${responseJson.data[i].url}' target="_blank">${responseJson.data[i].url}</a></p>
+      </li>`
+    );
+
+};
+
+$('#results').removeClass('hidden');
+};
+
+
+
+function getParks(query, maxResults) {
+  const params = {
+    api_key: apiKey,
+    q: query,
+    limit: maxResults,
+    fields: "addresses",
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const stateName = $('#js-state-name').val();
+    const maxResults = $('#js-max-results').val();
+    getParks(stateName, maxResults - 1);
+  });
+}
+
+$(watchForm);
